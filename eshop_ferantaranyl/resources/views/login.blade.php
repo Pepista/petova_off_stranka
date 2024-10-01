c31d45343225458b0d048bde9d6f8dd4ae4b4bce
Pep1sta <pcechak07@gmail.com>
2024-10-01 08:34:42 +0200

    blal

---
+++ eshop_ferantaranyl/resources/views/login.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ferantaranyl</title>
+</head>
+<body>
+    <h1>BAKA!!!</h1>
+    <h2>King Crimson > King Crimson Requiem</h2>
+</body>
+</html>
---
+++ git_config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git config --global user.name "Pep1sta"
+git config --global user.email "pcechak07@gmail.com"
+echo "Přihlášení uživatele proběhlo v pořádku"
+#cd eshop_ferantaranyl
+# php artisan serve
