39a53a4935d7b791a1e94c971a6d3074a5a4cf4e
Pep1sta <pcechak07@gmail.com>
2024-10-14 07:43:29 +0200

    feat: :bug: idk
    ne
    
    yabadabado

--- eshop_ferantaranyl/database/seeders/ProductSeeder.php
+++ eshop_ferantaranyl/database/seeders/ProductSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+use Faker\Factory as Faker; 
 
 class ProductSeeder extends Seeder
 {
@@ -13,12 +14,14 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         $faker = Faker::create();
-        for ($i=0; $i < 10; $i++) {         
-            DB::table('products')->insert([ 
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('products')->insert([
                 'name' => $faker->word,
                 'description' => $faker->sentence,
                 'price' => $faker->randomFloat(2, 50, 1000),
                 'stock' => $faker->numberBetween(1, 100),
+                'category' => $faker->word, 
+            ]);
         }
     }
 }
--- git_config.sh
+++ git_config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-git config --global user.name "Pep1sta"
+git config --global user.name "Pepista"
 git config --global user.email "pcechak07@gmail.com"
 echo "Přihlášení uživatele proběhlo v pořádku"
+
 #cd eshop_ferantaranyl
 # php artisan serve
