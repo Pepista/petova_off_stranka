5844ed6bc878f80860d57a98c873c38e92c0a07b
Pep1sta <pcechak07@gmail.com>
2024-10-14 08:17:47 +0200

    Funguje

--- eshop_ferantaranyl/database/seeders/ProductSeeder.php
+++ eshop_ferantaranyl/database/seeders/ProductSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker; 
 
 class ProductSeeder extends Seeder
